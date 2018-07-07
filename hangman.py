2cc3940c3eafb31556b5ca5d67d6bfe6afc99608
James Good <james.s.good@gmail.com>
2018-07-07 17:33:48 -0500

    Replaced integer counters with guess_log and a Counter object. re-worked instructions to be more clear. adjusted color scheme. no longer logging blank guesses.

--- hangman.py
+++ hangman.py
@@ -3,6 +3,7 @@
 # James Good 6/27/2018
 import pickle
 import random
+from collections import Counter
 
 import colorama
 from colorama import Fore, Back, Style
@@ -22,82 +23,73 @@ mystery_list = list(mystery_word)
 mystery_len = len(mystery_list)
 found_list = list('_' * mystery_len)
 guess_log = list()
+status_log = list()
 
-found = 0
-guesses = 0
-correct = 0
-incorrect = 0
-duplicate = 0
-invalid = 0
-blank = 0
-correct_word = 0
-incorrect_word = 0
-prompt = 'Guess a letter.'
-
-print(f'\nGuess one letter at a time, or try to guess the whole word at any time.\n\n'
-      f'Type {Style.BRIGHT}{Fore.YELLOW}exit{Style.RESET_ALL} to quit.\n\n'
-      f'The mystery word contains {Style.BRIGHT}{Fore.GREEN}{Back.BLACK}{mystery_len}{Style.RESET_ALL} letters.')
-
-while found < mystery_len:
-    print(f'{Style.BRIGHT}{Fore.CYAN}{Back.BLACK}[ {" ".join(found_list)} ]{Style.RESET_ALL}  {prompt} > ', end='')
+print(f'\n{Style.BRIGHT}{Fore.CYAN}Guess the Mystery Word!{Style.RESET_ALL}\n'
+      f'You can one letter at a time, or try to guess the whole word at any time.\n\n'
+      f'Type {Style.BRIGHT}{Fore.CYAN}exit{Style.RESET_ALL} to quit.\n\n'
+      f'The mystery word contains {Style.BRIGHT}{Fore.CYAN}{Back.BLACK}{mystery_len}{Style.RESET_ALL} letters.')
+print(f'{Style.BRIGHT}{Fore.GREEN}{Back.BLACK}[ {" ".join(found_list)} ]{Style.RESET_ALL}  '
+      f'{Style.BRIGHT}{Fore.CYAN}<-- Correct guesses will populate the Mystery Word here.')
+prompt = 'Guess a letter, or guess the word.'
+while found_list != mystery_list:
+    print(f'{Style.BRIGHT}{Fore.GREEN}{Back.BLACK}[ {" ".join(found_list)} ]{Style.RESET_ALL}  {prompt} > ', end='')
     guess = str(input()).strip()
-    guesses += 1
     if str(guess).isalpha():
         guess = str(guess).upper()
         if guess in guess_log:
-            duplicate += 1
-            prompt = f'You already guessed {Style.BRIGHT}{Fore.YELLOW}{guess}{Style.RESET_ALL}.'
             guess_log.append(guess)
+            status_log.append('duplicate') 
+            prompt = f'You already guessed {Style.BRIGHT}{Fore.YELLOW}{guess}{Style.RESET_ALL}.'
             continue
-        guess_log.append(guess)
         if len(guess) == 1:
+            guess_log.append(guess)
             if guess in mystery_list:
-                for index, s in enumerate(mystery_list):
-                    if s == guess:
+                for index, letter in enumerate(mystery_list):
+                    if letter == guess:
                         found_list[index] = guess
-                        found += 1
-                correct += 1
+                status_log.append('correct')
                 prompt = f'Correct!! {Style.BRIGHT}{Fore.GREEN}{guess}{Style.RESET_ALL} was a match!'
             else:
-                incorrect += 1
+                status_log.append('incorrect') 
                 prompt = f'Sorry, no {Style.BRIGHT}{Fore.YELLOW}{guess}{Style.RESET_ALL}. Try again.'
         elif len(guess) > 1:
             if guess == mystery_word:
-                correct_word += 1
+                guess_log.append(guess)
+                status_log.append('correct_word')
                 break
             elif guess == 'EXIT':
                 print(f'\n{Style.BRIGHT}{Fore.BLUE}Goodbye.')
                 colorama.deinit()
                 quit()
             else:
-                incorrect_word += 1
+                guess_log.append(guess)
+                status_log.append('incorrect_word')
                 prompt = f'Sorry, {Style.BRIGHT}{Fore.YELLOW}{guess}{Style.RESET_ALL} is not the mystery word.'
                 continue
     else:
         if len(guess) == 0:
-            blank += 1
             prompt = f'{Style.BRIGHT}{Fore.RED}No input detected.'
             continue
         else:
             prompt = f'{Style.BRIGHT}{Fore.RED}{guess}{Style.RESET_ALL} is not a valid input.'
-            invalid += 1
+            status_log.append('invalid')
             guess_log.append(guess)
             continue
 
-print(f'{Style.BRIGHT}{Fore.CYAN}{Back.BLACK}[ {" ".join(mystery_list)} ]{Style.RESET_ALL}  '
-      f'You guessed it! The mystery word was {Style.BRIGHT}{Fore.GREEN}{mystery_word}!\n')
+print(f'{Style.BRIGHT}{Fore.GREEN}{Back.BLACK}[ {" ".join(mystery_list)} ]{Style.RESET_ALL}  '
+      f'You guessed it! The mystery word was {Style.BRIGHT}{Fore.GREEN}{mystery_word}{Style.RESET_ALL}!\n')
 
-print(f'It took you {Style.BRIGHT}{Fore.CYAN}{guesses}{Style.RESET_ALL} guesses\'s.\n'
-      f' {Style.BRIGHT}{Fore.GREEN}{correct_word} correct word guesses.\n'
-      f' {Style.BRIGHT}{Fore.GREEN}{correct} correct letter guesses.\n'
-      f' {Style.BRIGHT}{Fore.YELLOW}{incorrect_word} incorrect word guesses.\n'
-      f' {Style.BRIGHT}{Fore.YELLOW}{incorrect} incorrect letter guesses.\n'
-      f' {Style.BRIGHT}{Fore.YELLOW}{duplicate} duplicate guesses.\n'
-      f' {Style.BRIGHT}{Fore.RED}{invalid} invalid guesses.\n'
-      f' {Style.BRIGHT}{Fore.RED}{blank} blank guesses.{Style.RESET_ALL}\n\n'
-      f'Guess Log: {Style.BRIGHT}{Fore.YELLOW}{" ".join(guess_log)}\n')
+count = Counter(status_log)
+print(f'It took you {Style.BRIGHT}{Fore.CYAN}{len(status_log)}{Style.RESET_ALL} guesses\'s.\n'
+      f' {Style.BRIGHT}{Fore.GREEN}{count["correct_word"]} correct word guesses.\n'
+      f' {Style.BRIGHT}{Fore.GREEN}{count["correct"]} correct letter guesses.\n'
+      f' {Style.BRIGHT}{Fore.YELLOW}{count["incorrect_word"]} incorrect word guesses.\n'
+      f' {Style.BRIGHT}{Fore.YELLOW}{count["incorrect"]} incorrect letter guesses.\n'
+      f' {Style.BRIGHT}{Fore.YELLOW}{count["duplicate"]} duplicate guesses.\n'
+      f' {Style.BRIGHT}{Fore.RED}{count["invalid"]} invalid guesses.{Style.RESET_ALL}\n')
 
-colorama.deinit()
+print(f'Guess Log: {Style.BRIGHT}{Fore.YELLOW}{" ".join(guess_log)}\n')
 
 if not word_list:
     word_list = random.sample(master_word_list, len(master_word_list))
@@ -107,3 +99,5 @@ try:
         pickle.dump(word_list, p_file)
 except:
     print(f'{Style.BRIGHT}{Fore.YELLOW}Error saving word list.')
+
+colorama.deinit()
