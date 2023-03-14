6671605cd02c97a464dbe4436fc1bfde6f1bedac
James Good <40248573+JSGood69@users.noreply.github.com>
2023-03-14 02:12:53 +0000

    new file:   game.py

---
+++ game.py
@@ -0,0 +1,10 @@
+# Author: James Good
+# Date: 2023-03-12
+# Description: This is the main game file for the Mystery Word game.
+
+from wordlist import WordList
+catlist = WordList().categories  
+print(catlist)
+
+
+  
